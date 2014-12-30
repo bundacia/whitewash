4a60787ec2da299566367b6c8471c3d0fed20a93
Trevor Little <trevor.little@livingsocial.com>
2014-12-30 17:03:52 -0500

    fixing typo

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject whitewash "0.1.0-SNAPSHOT"
-  :description "Clojure library for dencoding text as whitespace"
+  :description "Clojure library for encoding text as whitespace"
   :url "https://github.com/bundacia/whitewash"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/MIT"}
