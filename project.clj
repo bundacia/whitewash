4247dc777a55f02b38eaf0620b399c6fb4ffe284
Trevor Little <trevor.little@livingsocial.com>
2014-12-31 09:30:20 -0500

    initial commit of gh-pages

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject whitewash "0.1.0-SNAPSHOT"
-  :description "Clojure library for encoding text as whitespace"
-  :url "https://github.com/bundacia/whitewash"
-  :license {:name "MIT License"
-            :url "http://www.opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
--- test/whitewash/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns whitewash.core-test
-  (:require [clojure.test :refer :all]
-            [whitewash.core :refer :all]))
-
-(deftest washing
-  (testing "wash and unwash are symetric"
-    (is (= (unwash (wash "test string")) "test string")))
-  (testing "wash returns a whitespace only string"
-    (is (not (re-find #"\S" (wash "test string"))))))
